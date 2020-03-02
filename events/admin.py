5aaa79e0100a6a68e19a5cccdc43d841ed696e80
Ewan Hempsey <2314230h@student.gla.ac.uk>
2020-03-02 12:59:38 +0000

    ModelCreation

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from events.models import Category, Event, Comment, User
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Event)
+admin.site.register(Comment) 
+admin.site.register(User)
