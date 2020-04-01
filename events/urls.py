96a590f1bf43559e167712fd80fa59f08683eeb2
Rory McClymont <2376314m@student.gla.ac.uk>
2020-04-01 14:52:30 +0100

    Checks for login when visiting user views.

--- events/urls.py
+++ events/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
     path('sign-up', views.signup, name='sign-up'),
-    path('login', views.login, name='login'),
+    path('login/', views.login, name='login'),
 	path('logout/', views.user_logout, name= 'logout'),
     path('add_event', views.add_event, name='add_event'),
     path('event/<int:id>/<slug:event_slug>/', views.show_event, name='show_event'),
@@ -15,6 +15,6 @@ urlpatterns = [
     path('manage_events', views.manage_events, name='manage_events'),
     path('event/<int:id>/edit', views.edit_event, name='edit_event'),
     path('event/<int:id>/delete', views.delete_event, name='delete_event'),
-    path('events/account', views.edit_profile, name='profile'),
-    path('password/', views.change_password, name='change_password'),
+    path('edit_profile', views.edit_profile, name='profile'),
+    path('password', views.change_password, name='change_password'),
 ]
